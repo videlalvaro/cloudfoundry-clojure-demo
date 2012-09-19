bc6a53f6676fe04587cbd6df7e9ba232fd6f18c8
Alvaro Videla <videlalvaro@gmail.com>
2012-09-19 20:18:41 +0200

    improves description

--- src/clojure_demo/views/services.clj
+++ src/clojure_demo/views/services.clj
@@ -2,6 +2,7 @@
   (:require [clojure-demo.views.common :as common]
             [claude mongodb rabbitmq postgresql redis blob mysql])
   (:use [noir.core :only [defpage defpartial]]
+        hiccup.element
         claude.core))
 
 (defn get-service-prop [service prop]
@@ -48,13 +49,38 @@
                 ["name" "host" "hostname" "port" "user"
                  "username" "password"]))
 
+(defpartial reasons []
+  [:p ""]
+  [:p "We have created a library called "
+   (link-to "https://github.com/videlalvaro/claude" "claude")
+   " that lets you use "
+   (link-to "http://clojure.org/" "Clojure")
+   " in "
+   (link-to "http://cloudfoundry.com/" "Cloud Foundry")
+   ". This library is not official and is a first step into supporting "
+   "Clojure in Cloud Foundry."]
+  [:p "To use it simply add the following to your lein project dependencies: "
+   [:code "[claude \"0.2.1-snapshot\"]"]
+   " and then you'll be able to access directly in your code the credentials for the services
+bound to your application."]
+  [:p [:em "Note that this library only returns the information for the first"
+       " service of its kind. This is a limitation of this library not of Cloud Foundry."]]
+  [:p "To learn more how to deploy your "
+   [:strong "Clojure"]
+   " apps to "
+   [:strong "Cloud Foundry"]
+   " please visit the project page: "
+   (link-to "https://github.com/videlalvaro/cloudfoundry-clojure-demo"
+            "Cloud Foundry Clojure Demo")]
+  [:p "Bellow you have an example of the available functions and what they return for
+each of the services bound to this particular app. The data you see there is real from this website."])
+
 (defpage "/" []
-  (common/layout
+  (common/layout "home"
    [:div {:class "span12"}
     [:div {:class "hero-unit"}
-     [:h1 "Clojure - Cloud Foundry"]
-     [:p "Explaing Clojure usage in Cloud Foundry"]
-     [:p [:a {:class "btn btn-primary btn-large"} "Learn More"]]
+     [:h1 "Clojure in Cloud Foundry"]
+     (reasons)
      ] ;; hero-unit
     (mongodb-info)
     (rabbitmq-info)

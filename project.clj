1b97fbd24650351b0e71a677884709d11b08f060
Alvaro Videla <videlalvaro@gmail.com>
2012-09-19 21:19:18 +0200

    updates app description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-demo "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+  :description "Clojure - Cloud Foundry demo app"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta3"]
                  [claude "0.2.1-SNAPSHOT"]]

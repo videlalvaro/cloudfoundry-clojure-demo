e09c4987459cad36ff88b75364159ab2e4410dbb
Alvaro Videla <videlalvaro@gmail.com>
2012-09-19 20:18:51 +0200

    updates deps

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write this!"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta3"]
-                 [claude "0.2.0-snapshot"]]
+                 [claude "0.2.1-SNAPSHOT"]]
   :profiles {:dev {:resource-paths ["test/resources"]}}
   :main clojure-demo.server
   :plugins [[lein-swank "1.4.4"]])
